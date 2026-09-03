cff6e4ea49733fa0b457bbdb429d9da5756fad0d
Zack Russell <zackrussell49@gmail.com>
2026-09-02 22:09:03 -0500

    initial commit

---
+++ nlp_seq2seq.py
@@ -0,0 +1,7 @@
+'''
+This file is a first pass at NLP, specifically focusing on sequence to sequence (seq2seq) modeling
+
+This ties into work in my master's program, though is rudementary compared to what might 
+be introduced in production enviornments
+'''
+
